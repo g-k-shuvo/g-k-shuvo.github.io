145adb4f70490b95b0a075c1c3be6a5aaf8c5887
g-k-shuvo <golam.kibria@techjays.com>
2024-06-12 00:49:55 +0600

    SEO(Head): added seo tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,8 +2,22 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta title="Golam Kibria's Personal Website" />
+        <meta
+          name='description'
+          content="Golam Kibria's personal website. A software engineer based in Sylhet, Bangladesh."
+        />
+        <meta name='author' content='Golam Kibria' />
+        <meta
+          name='keywords'
+          content='software engineer, web developer, freelancer, frontend developer, backend developer, full stack developer '
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
