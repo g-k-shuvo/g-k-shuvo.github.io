665c8e96a8043d5237b9a2e1def097e386df4985
Golam Kibria <45451363+g-k-shuvo@users.noreply.github.com>
2026-01-04 17:28:45 +0600

    Integrate Google Analytics in _app.tsx
    Added Google Analytics scripts to the app component.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-NN305RCWB8"
+      />
+      <Script id="gtag-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-NN305RCWB8');
+        `}
+      </Script>
+
+      <Component {...pageProps} />
+    </>
+  );
 }
